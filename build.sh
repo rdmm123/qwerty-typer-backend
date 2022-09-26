7cef0f8243e6d3c2b1e8b4c0c9003779335fadb7
rdmm123 <robedanilo2001@gmail.com>
2022-09-25 22:08:25 -0500

    Changes for deployment

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
+# exit on error
+set -o errexit
 # Make migrations and migrate
 pip install -r requirements.txt
 
-python manage.py makemigrations --no-input
-mkmg_status=$?
-if [ $mkmg_status -ne 0 ]
-then
-  exit $mkmg_status
-fi
+# python manage.py makemigrations --no-input
+# mkmg_status=$?
+# if [ $mkmg_status -ne 0 ]
+# then
+#   exit $mkmg_status
+# fi
 python manage.py migrate --no-input
